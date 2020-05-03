a22c9cdb2c5d2de6d5dd023656fdda63310c8211
Difei Gu <gudifeifriend@gmail.com>
2020-05-03 10:03:59 -0400

    Actually add dashboard components

---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import AppDock from "./AppDock"
+
+export class dashboard extends Component {
+    render() {
+        return (
+            <div className="App">
+                <AppDock />
+            </div>
+        )
+    }
+}
+
+export default dashboard
